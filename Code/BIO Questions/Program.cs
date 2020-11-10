710a102bb8d31927b6d6a1ab4889d16ca0c72eb0
thatonearchguy <karandubey2911@gmail.com>
2020-11-10 12:50:09 +0000

    Added queue implementation and started shunting algorithm

---
+++ Code/BIO Questions/Program.cs
@@ -0,0 +1,50 @@
+﻿﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace BIO_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number: ");
+            var userInput = Console.ReadLine().ToString();
+            Console.WriteLine(FastPalindrome(userInput));
+        }
+        public static char[] NumToString(Int64 number)
+        {
+            return number.ToString().ToCharArray();
+        }
+        public static bool IsPalindrome(Int64 input)
+        {
+            while(true)
+            {
+                var numberArray = NumToString(input);
+                var duplicatedArray = NumToString(input);
+                Array.Reverse(numberArray);
+                if(duplicatedArray.SequenceEqual(numberArray) == false) return false;
+                else return true;
+            }
+        }
+        public static Int64 FastPalindrome(string input)
+        {
+            var number = Convert.ToInt64(input)+1;
+            var splitList = new List<int>();
+            if (number<9) return number;
+            else if (IsPalindrome(number) == true) return number;
+            else
+            {
+                foreach(char i in number.ToString()) splitList.Add(Convert.ToInt32(i.ToString()));
+                for (int i = 0; i < splitList.Count()/2; i ++)
+                {
+                    while (splitList[i] != splitList[splitList.Count()-1-i])
+                    {
+                        splitList[splitList.Count()-1-i] = (splitList[splitList.Count()-1-i] + 1)%10;
+                    }
+                }
+            }
+            return Int64.Parse(String.Join("", splitList));
+        }
+    }
+}
--- Code/Stacks/Week9Stacks.cs
+++ Code/Stacks/Week9Stacks.cs
@@ -18,6 +18,92 @@ namespace Code.Stacks
             aStack.Pop();
             aStack.Pop();
         }
+        public static int ShuntingAlgorithm(string input)
+        {
+            var workQueue = new Queue<string>();
+            var workStack = new Stack<string>();
+            var operators = new List<string>{"+", "-", "x", "/", "(", ")"};
+            foreach(var token in input)
+            {
+                if (char.IsDigit(token)) workQueue.Add(token.ToString());
+                else
+                {
+                    if (workStack.IsEmpty()) workStack.Push(token.ToString());
+                    else
+                    {
+                        var index = operators.Find(n=>n=workStack.Pop());
+                        
+                    }
+                }
+            }
+        }
+        public static int RPN(string input)
+        {
+            var anotherStack = new Stack<string>();
+            foreach (var element in input)
+            {
+                if (char.IsDigit(element)) anotherStack.Push(element.ToString());
+                else
+                {
+                    var num1 = Int32.Parse(anotherStack.Pop());
+                    var num2 = Int32.Parse(anotherStack.Pop());
+                    switch (element)
+                    {
+                        case 'x':
+                            anotherStack.Push((num2*num1).ToString());
+                            break;
+                        case '/':
+                            anotherStack.Push((num2/num1).ToString());
+                            break;
+                        case '+':
+                            anotherStack.Push((num2+num1).ToString());
+                            break;
+                        case '-':
+                            anotherStack.Push((num2-num1).ToString());
+                            break;
+                    }
+                }
+            }
+            return Int32.Parse(anotherStack.Pop());
+        }
+    }
+    class Queue <T>
+    {
+        List<T> queue = new List<T>();
+        public bool isEmpty()
+        {
+            if (queue.Count == 0) return true;
+            else return false;
+        }
+        public T getFront()
+        {
+            return queue[0];
+        }
+        public void Add(T value)
+        {
+            queue.Add(value);
+        }
+        public T Dequeue()
+        {
+            if(isEmpty()) throw new System.InvalidOperationException("Queue is empty!");
+            else 
+            {
+                var values = queue[0];
+                queue.RemoveAt(0);
+                return values;
+            }
+        }
+        public void clear()
+        {
+            foreach (T value in queue)
+            {
+                queue.Remove(value);
+            }
+        }
+        public Queue()
+        {
+
+        }
     }
     class Stack <T>
     {
@@ -38,13 +124,18 @@ namespace Code.Stacks
         }
         public T Pop()
         {
-            if (stackPointer == -1) throw new System.InvalidOperationException("Stack is empty!");
+            if (IsEmpty()) throw new System.InvalidOperationException("Stack is empty!");
             else{
                 T value = stack[stackPointer];
                 stack[stackPointer] = default(T);
                 stackPointer -= 1;
                 return value;
             }
+        }
+        public bool IsEmpty()
+        {
+            if (stackPointer == -1) return true;
+            else return false;
         }
         public T Peek(int pointer)
         {
